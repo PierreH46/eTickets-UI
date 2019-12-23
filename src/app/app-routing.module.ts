f9f5ccaf3bd8888c658abeae457c9e186fcb76c0
pit46 <pit46h@gmail.com>
2019-12-23 18:42:24 +0100

    premier affichage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EticketsListComponent } from './etickets-list/etickets-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'etickets', pathMatch: 'full' },
+  {path: 'etickets', component: EticketsListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
