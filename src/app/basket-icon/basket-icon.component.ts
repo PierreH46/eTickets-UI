46335b070a30dab66d57420d4326b9de2d492d40
pit46 <pit46h@gmail.com>
2019-12-24 19:11:49 +0100

    creation basket-icon

---
+++ src/app/basket-icon/basket-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basket-icon',
+  templateUrl: './basket-icon.component.html',
+  styleUrls: ['./basket-icon.component.scss']
+})
+export class BasketIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
