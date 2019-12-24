d91fa6023bfb568732c0a98527cf49241dc01744
pit46 <pit46h@gmail.com>
2019-12-24 19:17:02 +0100

    creation du service basket

---
+++ src/app/services/basket.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Eticket } from '../model/eticket';
+import { EticketService } from './eticket.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasketService {
+
+  eticketInfo: Array<{ eticket: Eticket, quantity: number; }> = []; // tableau initialisé à vide
+  totalAmount: number;
+  totalEtickets: number;
+
+  constructor(private eticketService: EticketService) { }
+
+  addEticket(eticket: Eticket, qty: number = 1) {
+    // le produit en cours est-il déjà dans le panier ? à quelle position pour pouvoir le mettre à jour
+    const index = this.eticketInfo.findIndex(pInfo => pInfo.eticket.id === eticket.id);
+
+    if (index !== -1) { // trouvé
+      // const pInfo = this.eticketInfo[index]; // produit + quantité
+      this.eticketInfo[index].quantity += qty;
+    } else { // pas trouvé
+      this.eticketInfo.push( {eticket, quantity: qty});
+    }
+
+    // incremente le nombre total d'articles et recalcule le montant total
+    this.totalEtickets += qty;
+    this.totalAmount = this.totalAmount + (qty * eticket.price);
+
+    // @TODO : enregistre le panier
+
+  }
+
+  removeEticket(eticket: Eticket) {
+    // le produit en cours est-il déjà dans le panier ? à quelle position pour pouvoir le mettre à jour
+    const index = this.eticketInfo.findIndex(pInfo => pInfo.eticket.id === eticket.id);
+    if (index !== -1) { // trouvé
+      this.eticketInfo[index].quantity--;
+      if (this.eticketInfo[index].quantity === 0) {
+        this.eticketInfo.splice(index, 1);
+      }
+    }
+    // decremente le nombre total d'articles et recalcule le montant total
+    this.totalEtickets--;
+    this.totalAmount = this.totalAmount - eticket.price;
+
+    // @TODO : enregistre le panier
+
+  }
+}
