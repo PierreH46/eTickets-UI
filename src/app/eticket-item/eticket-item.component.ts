db17965b89a1ed8759321a4050c8e97be7f0f976
pit46 <pit46h@gmail.com>
2019-12-24 19:18:54 +0100

    affichage de boutons ajouter/retirer au panier

--- src/app/eticket-item/eticket-item.component.ts
+++ src/app/eticket-item/eticket-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Eticket } from '../model/eticket';
+import { BasketService } from '../services/basket.service';
 
 @Component({
   selector: 'app-eticket-item',
@@ -9,10 +10,29 @@ import { Eticket } from '../model/eticket';
 export class EticketItemComponent implements OnInit {
 
   @Input() eticket: Eticket;
-  
-  constructor() { }
+  numEticketItem: number;
+
+  constructor(private basketService: BasketService) { }
 
   ngOnInit() {
+    this.numEticketItem = 0; // ToDO - initialiser avec le panier !!
+  }
+
+  add(event: Event){
+    event.stopPropagation(); // bloqué la propagation du au fait d'avoir mis
+    //      [routerLink]="['/eticket', eticket.slug]" sur la <div> mère 
+    //      au lieu de <img> - pas propre => solution navigate ou
+    //      choisir que seule la photo permettra de passer à DetailEticket
+    // increment du compteur affiché
+    this.numEticketItem++;
+    // appel à la methode du service 
+    this.basketService.addEticket(this.eticket);
+  };
+
+  remove(event: Event){
+    event.stopPropagation();
+    this.numEticketItem--;
+    this.basketService.removeEticket(this.eticket);
   }
 
 }
