2d2090fda2a3fbc2e2f447d628c13b360ec4ac40
zuloloxi <go.zulos@gmail.com>
2020-01-03 08:52:16 +0100

    login component mock test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EticketsListComponent } from './etickets-list/etickets-list.component';
+import { AuthGuard } from './helpers/auth.guard';
+import { LoginComponent } from './login/login.component';
+
+
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'etickets', pathMatch: 'full' },
-  {path: 'etickets', component: EticketsListComponent },
+  { path: '', component: EticketsListComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+        // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+//  {path: '', redirectTo: 'etickets', pathMatch: 'full' },
+//  {path: 'etickets', component: EticketsListComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// used to create fake backend
+import { fakeBackendProvider } from './helpers/fake-backend';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +12,12 @@ import { EticketsListComponent } from './etickets-list/etickets-list.component';
 import { EticketItemComponent } from './eticket-item/eticket-item.component';
 import { BasketIconComponent } from './basket-icon/basket-icon.component';
 import { UserIconComponent } from './user-icon/user-icon.component';
+import { RadiobtnsComponent } from './radiobtns/radiobtns.component';
+import { BasicAuthInterceptor } from './helpers/basic-auth.interceptor';
+import { ErrorInterceptor } from './helpers/error.interceptor';
+import { LoginComponent } from './login/login.component';
+
+
 
 @NgModule({
   declarations: [
@@ -15,16 +25,24 @@ import { UserIconComponent } from './user-icon/user-icon.component';
     EticketsListComponent,
     EticketItemComponent,
     BasketIconComponent,
-    UserIconComponent
+    UserIconComponent,
+    LoginComponent,
+    RadiobtnsComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule
   ],
   providers: [
-    { provide: 'BACKEND_URL', useValue: 'http://localhost:3004'} // JsonServer
+    { provide: 'BACKEND_URL', useValue: 'http://localhost:3004'}, // JsonServer
     // { provide: 'BACKEND_URL', useValue: 'http://localhost:8080'}
+    { provide: HTTP_INTERCEPTORS, useClass: BasicAuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    // provider used to create fake backend
+    fakeBackendProvider
+
   ],
   bootstrap: [AppComponent]
 })
