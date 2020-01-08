eee3c7678c88f0a46f4c4546775270a43043e8d1
pit46 <pit46h@gmail.com>
2020-01-08 16:53:30 +0100

    relative-options

---
+++ src/app/model/relative-options.spec.ts
@@ -0,0 +1,7 @@
+import { RelativeOptions } from './relative-options';
+
+describe('RelativeOptions', () => {
+  it('should create an instance', () => {
+    expect(new RelativeOptions()).toBeTruthy();
+  });
+});
